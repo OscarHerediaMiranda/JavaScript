393a743e51c8105b2efee9d75d8b44933dfba464
Oscar Heredia <u201913874@upc.edu.pe>
2024-07-14 01:43:31 -0500

    add fetch

--- Proyecto Cursos Online/scripts/index.js
+++ Proyecto Cursos Online/scripts/index.js
@@ -25,27 +25,30 @@ const containerCourses = document.querySelector('.main__courses');
 
 ];*/
 
-const data = []
+let data = []
 
 async function getCourses () {
     const url = 'http://localhost:3000/courses'
-    const response = await fetch(url)
-    console.log(response)
+    const response =  await fetch(url)
+    //console.log(response)
+    const json = await response.json()
+    console.log(json)
+    data = json
+    data.forEach(course => {
+        containerCourses.innerHTML += `
+        <div class="card">
+            <img src="${course.img}" alt="" class="card__img">
+            <h3 class="card__title">${course.title}</h3>
+            <p class="card__paragraph"><strong>${course.subtitle}</strong> ${course.description} </p>
+            <div class="card__data">
+                <span  class="card__price"> S/ ${course.price}</span>
+                <span  class=""> ${course.tag} </span>
+            </div>
+        </div>
+        `
+    })
 }
 
 getCourses()
 
-data.forEach(course => {
-    containerCourses.innerHTML += `
-    <div class="card">
-        <img src="${course.img}" alt="" class="card__img">
-        <h3 class="card__title">${course.title}</h3>
-        <p class="card__paragraph"><strong>${course.subtitle}</strong> ${course.description} </p>
-        <div class="card__data">
-            <span  class="card__price"> S/ ${course.price}</span>
-            <span  class=""> ${course.tag} </span>
-        </div>
-    </div>
-    `
-})
  /* FUNCION PARA CAMBIAR EL COLOR DE LETRAS (ANGULAR -> ROJO, REACT -> AZUL, VUE -> VERDE) */
